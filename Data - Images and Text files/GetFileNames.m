4b3c68a4ab25e6858191493c4efd3cffbb6d5f54
“jack <jackcbergin@gmail.com>
2022-04-30 20:40:17 -0400

    Finished script

--- Data - Images and Text files/GetFileNames.m
+++ Data - Images and Text files/GetFileNames.m
@@ -1,12 +1,26 @@
+%% Gets all the coord file names function
+% Description: This function is used to get the names of every jpg and
+% coordinate txt file name so manual entry is not needed. From there the
+% txt file list is then parsed through and separated out into a list with
+% all of the transform txt files and a list with all of the coordinate
+% transforms.
+
 function [coordTxt, jpgNames] = GetFileNames()
+    % Gets the directory for the jpg and the txt files
     txtDir = dir('*.txt');
     jpgDir = dir('*.jpg');
-
+    
+    % Gets the names of all the txt and jpg files.
     txtNames = {txtDir.name};
     jpgNames = {jpgDir.name};
+    
+    % Initializes the two cell arrays we will need to parse through the
+    % list of txt files.
     coordTxt = {};
     transformTxt = {};
-
+    
+    % Parses through the entire list of txt files and then organzies based
+    % on coord or transform txt file
     for i = 1:609
         if (i/3-floor(i/3)) == 0
             transformTxt{i} = txtNames{i};
@@ -14,35 +28,10 @@ function [coordTxt, jpgNames] = GetFileNames()
             coordTxt{i} = txtNames{i};
         end
     end
+    % Removes all of the empty cells
     coordTxt = coordTxt(~cellfun('isempty',coordTxt));
     transformTxt = transformTxt(~cellfun('isempty',transformTxt));
 end
-
-%     txtDir = dir('*.txt');
-%     jpgDir = dir('*.jpg');
-% 
-%     txtNames = {txtDir.name};
-%     jpgNames = {jpgDir.name};
-%     coordTxt = {};
-%     transformTxt = {};
-% 
-%     for i = 1:609
-%         if (i/3-floor(i/3)) == 0
-%             transformTxt{i} = txtNames{i};
-%         else
-%             coordTxt{i} = txtNames{i};
-%         end
-%     end
-%     coordTxt = coordTxt(~cellfun('isempty',coordTxt));
-%     transformTxt = transformTxt(~cellfun('isempty',transformTxt));
-%    
-%     for j = 1:406
-%         disp(coordTxt(j));
-%     end 
-%     
-%     for k = 1:203
-%         disp(transformTxt(k));
-%     end
 
 
 
--- Data - Images and Text files/RunAll.m
+++ Data - Images and Text files/RunAll.m
@@ -1,6 +1,16 @@
-%% Set (1) - R
+%% Main function
+% Description: This will be the main function that runs the "DataParsing.m"
+% script (which graphs all of the points in the txt files on to the 
+% needles) and the "GetFileNames.m" script (which gets all of the names 
+%for the desired jpg files and coord txt files) for every one of the jpg 
+%and txt files.
+
+% This command gets and stores all of the file names in the variables
+% "txtNames" and "jpgNames"
 [txtNames, jpgNames] = GetFileNames();
 
+% This loop is used to run through the "DataParsing.m" script which parses
+% through the txt files and plots their points on the prospective jpg.
 for i = 1:406
     % Sets the file address and image address
     fileAddr = txtNames{i};
@@ -9,4 +19,5 @@ for i = 1:406
     % Calls the function which then creates a new figure and returns the text
     % information
     txt1 = DataParsing(fileAddr, imageAddr);
+    pause
 end
